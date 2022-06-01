263d98daa0f41b30a74f24bdc883e1e6f36942fb
Gala Thevenet <galathevenet@hotmail.com>
2022-06-01 11:15:35 +0200

    preventDefault added

--- app/javascript/controllers/animate_logo_controller.js
+++ app/javascript/controllers/animate_logo_controller.js
@@ -2,14 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["logo"]
+  static targets = ["logo", "link"]
 
   connect() {
     console.log("Hello from our first Stimulus controller")
   }
 
-  rotate(){
+  border(){
     this.logoTarget.classList.replace("static-logo", "animated")
+  }
+
+  rotate(){
+    console.log("preventDefault is working !")
+    preventDefault(e);{
+      e.preventDefault();
+    }
+    this.linkTarget.preventDefault();
+  }
+
+  preventDefault(event){
+    event.preventDefault()
   }
 
 }
